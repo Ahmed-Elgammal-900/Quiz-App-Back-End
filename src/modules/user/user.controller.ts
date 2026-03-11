0c91e5098ed460ae1dd1e3b19bd5469062c7b974
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2026-03-11 04:35:40 +0200

    feat(user): remove not essential  route

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Req } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { Get, Delete, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Response } from 'express';
 import { CurrentUser } from 'src/common/decorators/user.decorator';
 import { UserResponse } from '../auth/types/respnse-types';
-import { Public } from 'src/common/decorators/public.decorator';
 
 @Controller('user')
 export class UserController {
@@ -20,11 +19,5 @@ export class UserController {
     res.clearCookie('refresh_token');
 
     return message;
-  }
-
-  @Public()
-  @Get('hello')
-  hello(@Res({ passthrough: true }) res: Response) {
-    return { massege: 'hello' };
   }
 }
