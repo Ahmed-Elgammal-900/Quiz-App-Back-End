8641baed6a93a28f4e8fbe82d9d53e28f21f528b
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2026-03-16 05:18:07 +0200

    feat(quiz): add quiz, question, answer, user-progress and user-answer entities

---
+++ src/modules/quiz/entities/user-quiz-answer.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  Index,
+} from 'typeorm';
+import { Quiz } from './quiz.entity';
+import { Answer } from './answer.entity';
+import { Question } from './question.entity';
+import { User } from '../../user/entities/user.entity';
+
+@Entity('user_quiz_answers')
+@Index(['userId', 'quizId', 'questionId'])
+export class UserQuizAnswer {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'uuid', nullable: false })
+  userId: string;
+
+  @Column({ type: 'uuid', nullable: false })
+  quizId: string;
+
+  @Column({ type: 'uuid', nullable: false })
+  questionId: string;
+
+  @Column({ type: 'uuid', nullable: false })
+  selectedAnswerId: string;
+
+  @Column({ type: 'boolean', default: false })
+  isCorrect: boolean;
+
+  @ManyToOne(() => Quiz, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'quizId' })
+  quiz: Quiz;
+
+  @ManyToOne(() => Question, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'questionId' })
+  question: Question;
+
+  @ManyToOne(() => Answer, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'selectedAnswerId' })
+  selectedAnswer: Answer;
+
+  @ManyToOne(() => User, (user) => user.quizAnswers, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+}
