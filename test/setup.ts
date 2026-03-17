b1556973ee328a9baf3553ebc4f55fd15d23903e
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2026-03-18 00:59:04 +0200

    chore: Integerate CI with project

--- test/setup.ts
+++ test/setup.ts
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 process.env.NODE_ENV = 'test';
