e134c082dcdcb7e50ebe1d2f0db87016adb05918
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2026-03-18 03:10:32 +0200

    chore: load swagger css and js

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,14 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('docs', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.js',
+    ],
+  });
   //
   await app.listen(process.env.PORT ?? 3000);
 }
